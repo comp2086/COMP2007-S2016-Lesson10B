a225202db0fc9d05acd5cf88b005f7a53c6adfd6
Alex Andriishyn <alex.andriishyn@gmail.com>
2016-07-21 13:51:58 -0400

    Fix conflicts

--- COMP2007-S2016-Lesson10B/Models/Artist.cs
+++ COMP2007-S2016-Lesson10B/Models/Artist.cs
@@ -5,18 +5,9 @@ using System.Web;
 
 namespace COMP2007_S2016_Lesson10B.Models
 {
-<<<<<<< HEAD
 	public class Artist
 	{
 		public int ArtistId { get; set; }
 		public string Name { get; set; }
 	}
-=======
-    public class Artist
-    {
-        public int ArtistId { get; set; }
-        public string Name { get; set; }
-
-    }
->>>>>>> 24ad51de5f54811e5f26bddeab7d4e8b276f3af0
 }
